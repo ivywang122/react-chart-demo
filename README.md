472699f1cff633f61890111f41a98b3790400c7f
ivywang122 <mucha1202@gmail.com>
2019-04-23 17:10:44 +0800

    2019-04-23: Add github page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chart Demo
+
+[LIVE DEMO](https://ivywang122.github.io/react-chart-demo/)
---
+++ docs/precache-manifest.924ca0933bc91bc4e28e176cd7eb5591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d8b1c80e0f5d4d8830",
+    "url": "/react-chart-demo/static/js/runtime~main.cd58a9cd.js"
+  },
+  {
+    "revision": "4ba39993fadfb407e667",
+    "url": "/react-chart-demo/static/js/main.87f096f5.chunk.js"
+  },
+  {
+    "revision": "d1a204c9b1efcf897991",
+    "url": "/react-chart-demo/static/js/2.c1d690f4.chunk.js"
+  },
+  {
+    "revision": "4ba39993fadfb407e667",
+    "url": "/react-chart-demo/static/css/main.a17256f3.chunk.css"
+  },
+  {
+    "revision": "e3427073c473f5fd73da061396388da0",
+    "url": "/react-chart-demo/index.html"
+  }
+];
